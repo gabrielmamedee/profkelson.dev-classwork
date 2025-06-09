502302f8cd64a6561dde22ca61153c745987dc12
gabrielmamedee <mamede.dev@gmail.com>
2025-06-09 00:18:49 -0300

    Ajustes no exercicio

--- class05_exercicios/src/App.jsx
+++ class05_exercicios/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Layout from './components/Layout/Layout'
 import Contato from './pages/Contato/Contato'
+import Alunos from './pages/Alunos/Alunos'
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<Home />}/>
-            <Route path='/alunos' element={<Home />} />
+            <Route path='/alunos' element={<Alunos />} />
             <Route path='/cursos' element={<Home />} />
             <Route path='/contato' element={<Contato />} />
           </Route>
